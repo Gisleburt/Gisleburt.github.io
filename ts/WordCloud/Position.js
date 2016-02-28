e1a9b770e2334fcafbc347b79aa202be7a1e8153
Daniel Mason <daniel@danielmason.com>
2016-02-28 23:32:35 +0000

    Added blank constructor

--- ts/WordCloud/Position.js
+++ ts/WordCloud/Position.js
@@ -2,6 +2,10 @@ var WordCloud;
 (function (WordCloud) {
     var Position = (function () {
         function Position(x, y) {
+            if (typeof x == 'undefined') {
+                x = 0;
+                y = 0;
+            }
             if (x instanceof Position) {
                 y = x.y;
                 x = x.x;
--- ts/WordCloud/Position.ts
+++ ts/WordCloud/Position.ts
@@ -4,9 +4,14 @@ module WordCloud {
         x: number;
         y: number;
 
+        constructor();
         constructor(position: Position);
         constructor(x: number, y: number);
-        constructor(x: any, y?:number) {
+        constructor(x?: any, y?:number) {
+            if(typeof x == 'undefined') {
+                x = 0;
+                y = 0;
+            }
             if(x instanceof Position) {
                 y = x.y;
                 x = x.x;
