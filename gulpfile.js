60b3bb5f4c4ae73e0df51ddcdb6d2f6b7d08e734
Daniel Mason <daniel@danielmason.com>
2016-02-28 11:53:56 +0000

    Gulp+Sass

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
+gulp.task('watch', ['sass:watch']);
+
