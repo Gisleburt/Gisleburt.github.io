ecb34d431fb76e59de28d402b8e2c23c01c3453a
Daniel Mason <daniel@danielmason.com>
2016-03-09 23:07:02 +0000

    EaseScroll implementation

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,32 @@
+var EaseScroll;
+(function (EaseScroll) {
+    var Scroll = (function () {
+        function Scroll() {
+        }
+        Scroll.move = function (goal, timeToArrive) {
+            var now = Date.now();
+            var timeLeft = timeToArrive - now;
+            // Out of time
+            if (timeLeft <= Scroll.stepTime) {
+                window.scroll(0, goal);
+                return;
+            }
+            var distanceToMove = goal - window.pageYOffset;
+            var stepsRemaining = (timeToArrive - now) / Scroll.stepTime;
+            var distanceToMoveThisStep = distanceToMove / stepsRemaining;
+            window.scroll(0, window.pageYOffset + distanceToMoveThisStep);
+            window.setTimeout(Scroll.move, Scroll.stepTime, goal, timeToArrive);
+        };
+        Scroll.to = function (elementId, milliseconds) {
+            var goal = document.getElementById(elementId).offsetTop;
+            var timeToArrive = Date.now() + milliseconds;
+            window.setTimeout(Scroll.move, Scroll.stepTime, goal, timeToArrive);
+        };
+        Scroll.stepTime = 10;
+        return Scroll;
+    }());
+    EaseScroll.Scroll = Scroll;
+})(EaseScroll || (EaseScroll = {}));
 var WordCloud;
 (function (WordCloud) {
     var Position = (function () {
