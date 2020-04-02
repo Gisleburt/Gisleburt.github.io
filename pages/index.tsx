2563e8e60645925c923b468b95271fd5cda66dff
Daniel Mason <daniel@danielmason.com>
2020-04-03 00:03:48 +0100

    feat(style): added styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,12 @@ import FullWorkHistory from '../components/FullWorkHistory';
 import Skills from '../components/Skills';
 import PersonalStatement from '../components/PersonalStatement';
 import Title from '../components/Title';
+import StyleReset from '../components/StyleReset';
 
 
 const Home = (): JSX.Element => (
   <div className="container">
+    <StyleReset />
     <SiteHead />
     <Title />
     <PersonalStatement />
