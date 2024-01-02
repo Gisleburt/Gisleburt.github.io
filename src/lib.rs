5a733ee52e24d27c31e17ac4226f0317a40a5a31
Daniel Mason <daniel@danielmason.com>
2024-01-02 16:22:25 +0000

    add rough navigation

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,42 @@
 
 use dioxus::prelude::*;
 
-// create a component that renders a div with the text "Hello, world!"
+mod nav;
+
+use nav::*;
+
+fn Contact(cx: Scope) -> Element {
+    cx.render(rsx! {
+      Nav {
+            NavItem {href: "https://github.com/gisleburt", "GitHub"}
+            NavItem {href: "/Daniel Mason - CV With Matrix - 2024.pdf", "CV"}
+            NavItem {href: "https://www.linkedin.com/in/danieljamesmason/", "LinkedIn"}
+        }
+    })
+}
+
+fn Navigation(cx: Scope) -> Element {
+    cx.render(rsx! {
+      Nav {
+            NavItem {href: "#", "Home"}
+            NavItem {href: "#who-is-daniel", "Who is Daniel"}
+            NavItem {href: "#skills", "Skills"}
+            NavItem {href: "#presentations", "Presentations"}
+            NavItem {href: "#achievements", "Achievements"}
+            NavItem {href: "#personal-interests", "Personal Interests"}
+        }
+    })
+}
+fn Home(cx: Scope) -> Element {
+    cx.render(rsx! {
+        h1 {"Daniel // Mason"}
+        Contact {}
+        Navigation {}
+    })
+}
+
 pub fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {
-            "Hello, world!"
-        }
+        Home {}
     })
 }
---
+++ src/nav.rs
@@ -0,0 +1,29 @@
+use dioxus::prelude::*;
+
+
+#[derive(Props)]
+pub(super) struct NavItemProps<'a> {
+    href: &'a str,
+    children: Element<'a>
+}
+
+pub(super) fn NavItem<'a>(cx: Scope<'a, NavItemProps<'a>>) -> Element {
+    cx.render(rsx! {
+        li { a { href: "{cx.props.href}", &cx.props.children }}
+    })
+}
+
+#[derive(Props)]
+pub(super) struct NavProps<'a> {
+    children: Element<'a>
+}
+
+pub(super) fn Nav<'a>(cx: Scope<'a, NavProps<'a>>) -> Element {
+    cx.render(rsx! {
+        nav {
+            ol {
+                &cx.props.children
+            }
+        }
+    })
+}
