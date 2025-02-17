bd54995749ed4a00d9d9cecf5a914201a2a522f7
Daniel Mason <daniel@danielmason.com>
2025-02-17 22:42:49 +0000

    wip: start adding homelab page

--- src/pages/personal_interests/homelab.rs
+++ src/pages/personal_interests/homelab.rs
@@ -6,6 +6,18 @@ pub fn Homelab() -> Element {
     rsx! {
         Page {
             title: "Homelab",
+            section {
+                h2 { "A what now?" }
+            }
+            section {
+                h2 { "Hardware" }
+            }
+            section {
+                h2 { "Software" }
+            }
+            section {
+                h2 { "What's next?" }
+            }
         }
     }
 }
