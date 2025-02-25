78223dd93e4066173873772b3df8c40b6dbee266
Daniel Mason <daniel@danielmason.com>
2025-02-25 08:38:42 +0000

    fix: indentation

--- src/components/scrollbar.rs
+++ src/components/scrollbar.rs
@@ -4,19 +4,19 @@ use dioxus::prelude::*;
 pub fn Scrollbar() -> Element {
     document::eval(
         r##"
-        const container = document.querySelector(".page-container");
-        const scrollbar = document.querySelector(".scrollbar");
-        const shuttle = scrollbar.firstChild;
+            const container = document.querySelector(".page-container");
+            const scrollbar = document.querySelector(".scrollbar");
+            const shuttle = scrollbar.firstChild;
 
 
-        container.addEventListener("scroll", (event) => {
-          const height = scrollbar.scrollHeight;
-          const proportion = container.scrollTop / container.scrollTopMax;
-          const scrollMax = scrollbar.scrollHeight - (2 * shuttle.scrollHeight);
-          const newTop = scrollMax * proportion;
-          shuttle.style.top = `${newTop}px`;
-        });
-    "##,
+            container.addEventListener("scroll", (event) => {
+              const height = scrollbar.scrollHeight;
+              const proportion = container.scrollTop / container.scrollTopMax;
+              const scrollMax = scrollbar.scrollHeight - (2 * shuttle.scrollHeight);
+              const newTop = scrollMax * proportion;
+              shuttle.style.top = `${newTop}px`;
+            });
+        "##,
     );
     rsx! {
         div {
