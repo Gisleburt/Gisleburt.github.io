a77d795f80a2196eda7e934dc706fd6dbb17b69e
Daniel Mason <daniel@danielmason.com>
2025-03-10 19:17:50 +0000

    feat: download cv of pub gpg key

--- src/components/open.rs
+++ src/components/open.rs
@@ -1,5 +1,49 @@
 use crate::components::page::{ShownModal, MODAL_SIGNAL};
+use crate::utils::make_string_safe;
+use dioxus::logger::tracing;
 use dioxus::prelude::*;
+use std::string::ToString;
+
+#[derive(Clone, PartialEq)]
+struct Download {
+    file: Asset,
+    filename: &'static str,
+}
+
+const DANIEL_MASON_CV: Download = Download {
+    file: asset!("/assets/downloads/Daniel Mason CV.pdf"),
+    filename: "Daniel Mason CV.pdf",
+};
+
+const DANIEL_MASON_GPG: Download = Download {
+    file: asset!("/assets/downloads/danielmason.gpg.pub"),
+    filename: "danielmason.gpg.pub",
+};
+
+#[component]
+fn FileSelector(
+    download: &'static Download,
+    mut download_signal: Signal<Option<&'static Download>>,
+) -> Element {
+    let id = make_string_safe(download.filename);
+    let filename = download.filename;
+    rsx! {
+        li {
+            class: "file-select",
+            input {
+                type: "radio",
+                id: "{id}",
+                name: "file-select",
+                value: "{filename}",
+                checked: false,
+                onchange: move |_| {
+                    download_signal.set(Some(download));
+                }
+            }
+            label { for: "{id}", "{filename}" }
+        }
+    }
+}
 
 #[component]
 pub fn Open() -> Element {
@@ -7,13 +51,33 @@ pub fn Open() -> Element {
         .then(|| "show")
         .unwrap_or_default();
 
+    let download_signal: Signal<Option<&'static Download>> = use_signal(|| None);
+    let download_value: Option<&Download> = download_signal();
+    let download_disabled_class = download_value
+        .is_none()
+        .then(|| "disabled")
+        .unwrap_or_default();
+    let download_link = download_value
+        .map(|download| format!("{}", download.file))
+        .unwrap_or_else(|| "#".to_string());
+    let filename = download_value
+        .map(|download| download.filename)
+        .unwrap_or_else(|| "");
+
     rsx! {
         section {
             class: "open modal {show_class}",
             div {
                 h2 { "Open" }
                 div {
-                    label { "File Name: " input { type: "input" } }
+                    label {
+                        "File Name: "
+                        input {
+                            type: "input",
+                            disabled: download_signal.read().is_none(),
+                            value: "{filename}"
+                        }
+                    }
                 }
                 div {
                     "D:\\"
@@ -23,7 +87,8 @@ pub fn Open() -> Element {
                     div {
                         h2 { "Files" }
                         ul {
-                            li { "Daniel Mason CV.pdf" }
+                            FileSelector { download: &DANIEL_MASON_CV, download_signal }
+                            FileSelector { download: &DANIEL_MASON_GPG, download_signal }
                         }
                     }
                     div {
@@ -36,9 +101,9 @@ pub fn Open() -> Element {
                 div {
                     class: "buttons",
                     a {
-                        class: "button",
-                        href: "#",
-                        onclick: move |_| *MODAL_SIGNAL.write() = ShownModal::None,
+                        class: "button {download_disabled_class}",
+                        download: filename,
+                        href: download_link,
                         "OK"
                     }
                     a {
